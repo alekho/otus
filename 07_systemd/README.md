8870b1fbf362bf05e4a92d55402e46920c1d8811
alekho <khorev.aleksandr@gmail.com>
2020-05-31 14:03:02 +0300

    part2 v2

--- 07_systemd/README.md
+++ 07_systemd/README.md
@@ -7,3 +7,14 @@ https://habr.com/ru/company/ruvds/blog/326328/
 http://rus-linux.net/nlib.php?name=/MyLDP/consol/HuMan/logger-ru.html
 
 https://wiki.enchtex.info/doc/bash/redirectes
+
+
+
+/etc/init.d/spawn-fcgi
+
+```bash
+exec="/usr/bin/spawn-fcgi"
+prog="spawn-fcgi"
+config="/etc/sysconfig/spawn-fcgi"
+```
+
--- 07_systemd/systemd.sh
+++ 07_systemd/systemd.sh
@@ -3,15 +3,33 @@
 mkdir -p ~root/.ssh
 cp ~vagrant/.ssh/auth* ~root/.ssh
 
+#1 часть
 #Делаем скрипт исполняемым
 chmod +x /vagrant/watcher/watcher.sh
 
+#Копируем необходимые файлы
 cp /vagrant/watcher/watcher.conf /etc/sysconfig
 cp /vagrant/watcher/test.log /var/log
 cp /vagrant/watcher/watcher.sh /opt/
-cp /vagrant/watcher/watcher.service /usr/lib/systemd/system
-cp /vagrant/watcher/watcher.timer /usr/lib/systemd/system
+cp /vagrant/watcher/watcher.service /etc/systemd/system
+cp /vagrant/watcher/watcher.timer /etc/systemd/system
 
-sudo systemctl daemon-reload
-sudo systemctl enable watcher.service watcher.timer
-sudo systemctl start watcher.service watcher.timer
+#Включаем автозапуск сервиса и стартуем
+systemctl daemon-reload
+systemctl enable watcher.service watcher.timer
+systemctl start watcher.service watcher.timer
+
+#2 часть
+#Устанавливаем необходимое spawn-fcgi - используется для запуска
+#процессов FastCGI, поэтому ставим приложение которое поддерживает данный интерфейс
+#в нашем случаи это httpd с модулем mod_fcgid
+yum install epel-release -y && yum install spawn-fcgi php php-cli mod_fcgid httpd -y
+#Кривенько разкоменнитруем конфиг spawn-fcgi
+grep \#SOCKET  /etc/sysconfig/spawn-fcgi | cut -c 2- >> /etc/sysconfig/spawn-fcgi 
+grep \#OPTION  /etc/sysconfig/spawn-fcgi | cut -c 2- >> /etc/sysconfig/spawn-fcgi 
+#Копируем файл юнита
+cp /vagrant/spawn-fcgi/spawn-fcgi.service /etc/systemd/system
+
+systemctl daemon-reload
+systemctl enable spawn-fcgi.service
+systemctl start spawn-fcgi.service
